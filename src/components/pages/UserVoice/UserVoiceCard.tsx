ace53861d90002dfd54bf929503d75f3bc7af39a
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2025-06-06 15:44:17 +0200

    record ios 03

--- src/components/pages/UserVoice/UserVoiceCard.tsx
+++ src/components/pages/UserVoice/UserVoiceCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "./UserVoice.module.css";
 import { useRecorder } from "./useRecorder";
